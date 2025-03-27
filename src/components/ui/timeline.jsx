3a1a6faf7386372024fb96e56a257cfb266b5924
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-03-27 22:05:09 +0530

    modified:   src/components/ui/timeline.jsx

--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -29,7 +29,7 @@ export const Timeline = ({
     <div
       className={`w-full bg-white dark:bg-neutral-950 font-sans md:px-10 relative overflow-hidden ${className}`}
       ref={containerRef}>
-      <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-10">
+      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
         <h2 className="text-2xl mb-2 md:text-4xl text-black dark:text-white max-w-4xl">
           My Journey
         </h2>

c9ba241d7d59affdadfee9a065b8571da2275f9b
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-03-27 22:27:12 +0530

    modified:   src/components/sections/ContactArea.jsx

--- src/components/sections/ContactArea.jsx
+++ src/components/sections/ContactArea.jsx
@@ -4,7 +4,7 @@ import { FlipText } from "../ui/FlipText";
 const ContactArea = () => {
   return (
     <div className="flex flex-col md:flex-row justify-between items-start p-12 mx-6 md:mx-60 gap-6">
-        <div className="flex flex-col justify-start items-start gap-2" >
+        <div className="flex flex-col justify-center gap-2" >
             <FlipText />
             <p>Geek out about tech, contact for collaborations, and more!</p>
         </div>

e3fa06e0c241ca10e4b4baf3aa608b6c3314152e
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-03-31 19:35:37 +0530

    Add ContactArea - AnimatedBeam

--- src/components/sections/ContactArea.jsx
+++ src/components/sections/ContactArea.jsx
@@ -1,12 +1,16 @@
 import Contact from "./Contact";
 import { FlipText } from "../ui/FlipText";
+import { ContactBeam } from "./ContactBeam";
 
 const ContactArea = () => {
   return (
     <div className="flex flex-col md:flex-row justify-between items-start p-12 mx-6 md:mx-60 gap-6">
-        <div className="flex flex-col justify-center gap-2" >
+        <div className="flex flex-col justify-center gap-2 h-full">
+          <div className="flex flex-col gap-2 mb-10 md:mb-20">
             <FlipText />
             <p>Geek out about tech, contact for collaborations, and more!</p>
+          </div>
+          <ContactBeam />
         </div>
         <Contact />
     </div>
---
+++ src/components/sections/ContactBeam.jsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React, { useRef } from "react";
+import { AnimatedBeam } from "../magicui/animated-beam";
+
+export function ContactBeam() {
+  const containerRef = useRef(null);
+  const div1Ref = useRef(null);
+  const div2Ref = useRef(null);
+
+  return (
+    <div
+      className="relative flex w-full max-w-[500px] items-center justify-center overflow-hidden"
+      ref={containerRef}
+    >
+      <div className="flex size-full flex-col items-stretch justify-between gap-10">
+        <div className="flex flex-row justify-between">
+          <div
+            ref={div1Ref}
+            className="z-10 flex size-12 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="black"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+              <circle cx="12" cy="7" r="4" />
+            </svg>
+          </div>
+          <div
+            ref={div2Ref}
+            className="z-10 flex size-12 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]"
+          >
+            {/* MessagesSquare SVG */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="black"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2z" />
+              <path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1" />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div1Ref}
+        toRef={div2Ref}
+        startYOffset={10}
+        endYOffset={10}
+        curvature={-20}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div1Ref}
+        toRef={div2Ref}
+        startYOffset={-10}
+        endYOffset={-10}
+        curvature={20}
+        reverse
+      />
+    </div>
+  );
+}

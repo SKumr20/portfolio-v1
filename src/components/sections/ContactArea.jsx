774d98e336bd9a467aee058ce6973e87dfdcbea1
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-12 23:10:01 +0530

    Add ContactArea gap spacing

--- src/components/sections/ContactArea.jsx
+++ src/components/sections/ContactArea.jsx
@@ -4,8 +4,8 @@ import { ContactBeam } from "./ContactBeam";
 
 const ContactArea = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center md:items-start p-12 mx-6 md:mx-60 gap-10 md:gap-6">
-        <div className="flex flex-shrink-0 flex-col justify-center gap-2 h-full">
+    <div className="flex flex-col md:flex-row justify-between items-center md:items-start p-12 mx-6 md:mx-60 gap-20 md:gap-6">
+        <div className="flex flex-shrink-0 flex-col justify-center gap-10 h-full">
           <div className="flex flex-col gap-2 mb-10 md:mb-20">
             <FlipText />
             <p>Geek out about tech, contact for collaborations, and more!</p>

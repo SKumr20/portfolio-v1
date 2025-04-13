4ac82df878823706de17615053a6da93a4fa350e
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-13 13:29:00 +0530

    Fix TechPins wrapping and margin

--- src/components/ui/TechPins.jsx
+++ src/components/ui/TechPins.jsx
@@ -6,9 +6,9 @@ import { BsFiletypeJava } from "react-icons/bs";
 
 const TechPins = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-4">
-        <h1 className="text-xl font-bold">More tech I'm familiar with: </h1>
-        <div className="flex justify-center items-center gap-2">
+    <div className="flex flex-col justify-center items-center gap-4 px-2">
+        <h1 className="text-xl font-bold">More tech I use: </h1>
+        <div className="flex justify-center items-center gap-2 flex-wrap">
             <Badge className="flex justify-center items-center gap-2">
                 <SiCplusplus className="scale-125"/>
                 C++
@@ -21,7 +21,7 @@ const TechPins = () => {
                 <FaPython className="scale-125"/>
                 Python
             </Badge>
-            <Badge className="flex justify-center items-center gap-2">
+            <Badge className="flex justify-center items-center gap-2 whitespace-nowrap">
                 <FiFramer className="scale-125"/>
                 Framer Motion
             </Badge>
@@ -30,4 +30,4 @@ const TechPins = () => {
   )
 }
 
-export default TechPins
+export default TechPins;

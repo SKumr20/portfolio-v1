26c101cf810cf3236bf6957896f5f92c283e6292
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-23 14:37:32 +0530

    Add animation to sections

--- src/components/sections/ContactArea.jsx
+++ src/components/sections/ContactArea.jsx
@@ -1,10 +1,20 @@
 import Contact from "./Contact";
 import { FlipText } from "../ui/FlipText";
 import { ContactBeam } from "./ContactBeam";
+import * as motion from "motion/react-client";
 
 const ContactArea = () => {
   return (
-    <div id="contactArea" className="flex flex-col md:flex-row justify-between items-center md:items-start p-12 mx-6 md:mx-60 gap-20 md:gap-6">
+    <motion.div
+    id="contactArea" 
+    initial={{ opacity: 0, y: 50, filter: 'blur(10px)' }}
+      whileInView={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
+      viewport={{ once: false, amount: 0.2 }}
+      transition={{
+        duration: 1,
+        ease: [0.43, 0.13, 0.23, 0.96]
+      }} 
+     className="flex flex-col md:flex-row justify-between items-center md:items-start p-12 mx-6 md:mx-60 gap-20 md:gap-6">
         <div className="flex flex-shrink-0 flex-col justify-center gap-10 h-full">
           <div className="flex flex-col gap-2 mb-10 md:mb-20">
             <FlipText />
@@ -16,7 +26,7 @@ const ContactArea = () => {
           <Contact />
         </div>
         
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/sections/TechStack.jsx
+++ src/components/sections/TechStack.jsx
@@ -1,16 +1,25 @@
 import { AuroraText } from "../magicui/aurora-text";
 import TechCards from "../ui/TechCards";
 import TechPins from "../ui/TechPins";
+import * as motion from "motion/react-client";
 
 const TechStack = () => {
   return (
-    <div className="flex flex-col items-center justify-center p-12 gap-12">
+    <motion.div
+    initial={{ opacity: 0, y: 50, filter: 'blur(10px)' }}
+      whileInView={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
+      viewport={{ once: false, amount: 0.2 }}
+      transition={{
+        duration: 1,
+        ease: [0.43, 0.13, 0.23, 0.96]
+      }} 
+    className="flex flex-col items-center justify-center p-12 gap-12">
         <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl">
             Tech <AuroraText>Stack</AuroraText>
         </h1>
         <TechCards />
         <TechPins />
-    </div>
+    </motion.div>
   )
 }
 
